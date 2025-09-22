87513f97b7eaa71405af46b40595c42ad7798d1e
gazalee <gazawan51@gmail.com>
2025-09-22 04:22:32 +0100

    Added nav bar and corresponding styles

--- src/shared/navbar.js
+++ src/shared/navbar.js
@@ -4,7 +4,7 @@ export function Navbar() {
 
   section.innerHTML = `
     <!--Logo and name-->
-    <section>
+    <section class="name-and-logo">
       <img src="/src/assets/logo1.png" alt="EatD Palace Logo">
       <p>EatD Palace</p>
     </section>
@@ -15,24 +15,33 @@ export function Navbar() {
           <iconify-icon icon="guidance:home-2"></iconify-icon>
           Home
         </li>
-        <li>Menu</li>
-        <li>About us</li>
-        <li>Reservation</li>
+        <li>
+            <iconify-icon icon="fluent:food-16-regular"></iconify-icon>
+            Menu
+        </li>
+        <li>
+            <iconify-icon icon="cil:info"></iconify-icon>
+            About us
+        </li>
+        <li>
+            <iconify-icon icon="fluent-mdl2:reservation-orders"></iconify-icon>
+            Reservation
+        </li>
       </ul>
     </nav>
 
     <!--Essentials-->
-    <section>
+    <section class="essentials">
       <search>
         <input type="search" name="search-for-items-in-menu" placeholder="Search...">
-        <button class="nav-buttons">
+        <button class="search nav-buttons">
           <iconify-icon icon="akar-icons:search"></iconify-icon>
         </button>
       </search>
-      <button class="nav-buttons">
+      <button class="acc nav-buttons">
         <iconify-icon icon="et:profile-male"></iconify-icon>
       </button>
-      <button class="nav-buttons">
+      <button class="acc nav-buttons">
         <iconify-icon icon="uil:cart"></iconify-icon>
       </button>
     </section>
